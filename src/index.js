412cf5b848acb1505e3f301af93b681e199b49be
artsiom.lazar <artsiom.lazar@afrans.by>
2021-03-20 20:34:00 +0300

    added test electron app

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const os = require('os-utils');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+    app.quit();
+}
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    setInterval(() => {
+        os.cpuUsage((v) => {
+            mainWindow.webContents.send('cpu', v * 100);
+            mainWindow.webContents.send('mem', os.freememPercentage() * 100);
+            mainWindow.webContents.send('total-mem', os.totalmem() / 1024);
+        });
+    }, 1000);
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
